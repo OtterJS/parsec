716fb5d4906d08f945625394d3f8872be1c7be14
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-08-16 17:53:29 +0100

    tooling: use `tsup` for building

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: {
+    index: 'src/index.ts'
+  },
+  format: ['esm'],
+  target: 'node20',
+  clean: true,
+  minify: false,
+  dts: true,
+  outDir: 'dist'
+})
