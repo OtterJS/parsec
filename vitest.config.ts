b87e6173bb5bfa5fc248b1585805ab695c379cde
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-08-16 17:07:27 +0100

    tooling: configure vitest coverage reporter

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,17 @@
-import path from "node:path";
+import path from 'node:path'
 
 export default {
+  test: {
+    environment: 'node',
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['lcov'],
+      include: ['src']
+    }
+  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-};
+}
