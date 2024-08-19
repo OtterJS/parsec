abefbfd203a3a568e0b87d544b5ceed65d831c18
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-08-19 01:47:51 +0100

    fix: return status code `413 Content Too Large` when limiter throws

--- src/utils/limiter.ts
+++ src/utils/limiter.ts
@@ -19,7 +19,7 @@ export class Limiter extends stream.Transform {
     this.currentLength += chunk.byteLength
     if (this.currentLength > this.lengthLimit) {
       return callback(new ClientError('Decoded content size exceeded limit', {
-        statusCode: 400,
+        statusCode: 413,
         code: "ERR_CONTENT_LENGTH_EXCEEDED_LIMIT"
       }))
     }
