9c5c655c535522c2307b9821e0de53bb038a6d8b
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-08-19 02:32:03 +0100

    feat: add options to `raw` parser

--- src/content-types/raw.ts
+++ src/content-types/raw.ts
@@ -1,13 +1,37 @@
-import { getRawRead } from '@/get-read'
+import {getRawRead, type RawReadOptions} from '@/get-read'
 import type { HasBody, MaybeParsed, NextFunction, Request, Response } from '@/types'
 import { alreadyParsed } from '@/utils/already-parsed-symbol'
 import { hasNoBody } from '@/utils/has-no-body'
 
-export const raw = () => {
-  const read = getRawRead()
-  return async (req: Request & HasBody<Buffer> & MaybeParsed, res: Response, next: NextFunction) => {
+export type RawBodyParsingOptions<
+  Req extends Request & HasBody<Buffer> = Request & HasBody<Buffer>,
+  Res extends Response<Req> = Response<Req>,
+> = RawReadOptions & {
+  /**
+   * Matcher used to determine which requests the middleware should body-parse.
+   *
+   * The default matcher will match all requests.
+   *
+   * @default () => true
+   */
+  matcher?: (req: Req, res: Res) => boolean
+}
+
+export function raw<
+  Req extends Request & HasBody<Buffer> = Request & HasBody<Buffer>,
+  Res extends Response<Req> = Response<Req>,
+>(options: RawBodyParsingOptions<Req, Res>) {
+  const optionsCopy: RawReadOptions = Object.assign({}, options)
+  optionsCopy.limit ??= '100kb'
+  optionsCopy.inflate ??= true
+  
+  const matcher = options?.matcher ?? (() => true)
+  
+  const read = getRawRead<Buffer>(options)
+  return async (req: Req & MaybeParsed, res: Res, next: NextFunction) => {
     if (req[alreadyParsed] === true) return next()
     if (hasNoBody(req.method)) return next()
+    if (!matcher(req, res)) return next()
     req.body = await read(req, res)
     next()
   }
