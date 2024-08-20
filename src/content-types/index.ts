48c09be39220206afa437dd5eef49c3ffad17dc0
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-08-20 15:52:35 +0100

    feat: add `multipart` body-parsers

--- src/content-types/index.ts
+++ src/content-types/index.ts
@@ -1,5 +1,7 @@
 export * from './custom'
 export * from './json'
+export * from "./multipart-form-data"
 export * from './raw'
+export * from "./raw-multipart"
 export * from './text'
 export * from './urlencoded'
---
+++ src/content-types/multipart-form-data.ts
@@ -0,0 +1,66 @@
+import { ContentType } from '@otterhttp/content-type'
+import { ClientError, HttpError } from '@otterhttp/errors'
+
+import { type RawReadOptions, type ReadOptions, getRawRead } from '@/get-read'
+import { type ParsedMultipartFormData, parseMultipartFormData } from '@/parsers/multipart-form-data'
+import type { HasBody, MaybeParsed, NextFunction, Request, Response } from '@/types'
+import { alreadyParsed } from '@/utils/already-parsed-symbol'
+import { hasNoBody } from '@/utils/has-no-body'
+import { typeChecker } from '@/utils/type-checker'
+
+export type MultipartFormDataBodyParsingOptions<
+  Req extends Request & HasBody<ParsedMultipartFormData> = Request & HasBody<ParsedMultipartFormData>,
+  Res extends Response<Req> = Response<Req>,
+> = RawReadOptions & {
+  /**
+   * Matcher used to determine which requests the middleware should body-parse.
+   *
+   * The default matcher will match requests with Content-Type `multipart/form-data`, `multipart/foobar+form-data`, etc.
+   *
+   * @default typeChecker("multipart/*+form-data")
+   */
+  matcher?: (req: Req, res: Res) => boolean
+}
+
+export function multipartFormData<
+  Req extends Request & HasBody<ParsedMultipartFormData> = Request & HasBody<ParsedMultipartFormData>,
+  Res extends Response<Req> = Response<Req>,
+>(options?: MultipartFormDataBodyParsingOptions<Req, Res>) {
+  const optionsCopy: ReadOptions = Object.assign({}, options)
+  optionsCopy.limit ??= '10mb'
+  optionsCopy.inflate ??= true
+
+  const matcher = options?.matcher ?? typeChecker(ContentType.parse('multipart/*+form-data'))
+
+  function failBoundaryParameter(): never {
+    throw new ClientError("multipart body cannot be parsed without 'boundary' parameter", {
+      statusCode: 400,
+      code: 'ERR_MULTIPART_BOUNDARY_REQUIRED',
+    })
+  }
+
+  const rawRead = getRawRead(options)
+  return async (req: Req & MaybeParsed, res: Res, next: NextFunction) => {
+    if (req[alreadyParsed] === true) return next()
+    if (hasNoBody(req.method)) return next()
+    if (!matcher(req, res)) return next()
+
+    if (req.headers['content-type'] == null) failBoundaryParameter()
+    const contentType = ContentType.parse(req.headers['content-type'])
+    if (!Object.hasOwn(contentType.parameters, 'boundary')) failBoundaryParameter()
+    const boundary = contentType.parameters.boundary
+
+    const rawBody = await rawRead(req, res)
+
+    try {
+      req.body = parseMultipartFormData(rawBody, boundary)
+    } catch (err) {
+      if (err instanceof HttpError) throw err
+      throw new ClientError('Multipart body parsing failed', {
+        statusCode: 400,
+        code: 'ERR_MULTIPART_PARSE_FAILED',
+        cause: err instanceof Error ? err : undefined,
+      })
+    }
+  }
+}
