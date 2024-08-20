9975310dc0c3f25d564712dff0c188ba7c0b298e
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-08-20 16:50:18 +0100

    test(parsers/multipart): add more tests to ensure RFC 2046 is adhered to

--- tests/parsers/multipart.test.ts
+++ tests/parsers/multipart.test.ts
@@ -57,3 +57,112 @@ it('should parse valid multipart with multiple parts', () => {
     },
   ])
 })
+
+it('should parse valid multipart form data with blank part', () => {
+  const multipart = [
+    'preamble',
+    '--boundary',
+    '',
+    '',
+    '--boundary--',
+    'epilogue',
+  ].join('\r\n')
+
+  expect(parseMultipart(Buffer.from(multipart), 'boundary')).toEqual([
+    {
+      headers: {},
+      content: Buffer.from(""),
+    }
+  ])
+})
+
+it('should parse valid multipart form data with no preamble', () => {
+  const multipart = [
+    '--boundary',
+    '',
+    '',
+    '--boundary--',
+    'epilogue',
+  ].join('\r\n')
+
+  expect(parseMultipart(Buffer.from(multipart), 'boundary')).toEqual([
+    {
+      headers: {},
+      content: Buffer.from(""),
+    }
+  ])
+})
+
+it('should parse valid multipart form data with no epilogue', () => {
+  const multipart = [
+    'preamble',
+    '--boundary',
+    '',
+    '',
+    '--boundary--',
+  ].join('\r\n')
+
+  expect(parseMultipart(Buffer.from(multipart), 'boundary')).toEqual([
+    {
+      headers: {},
+      content: Buffer.from(""),
+    }
+  ])
+})
+
+it('should reject multipart form data with missing CRLF to separate headerlines from content', () => {
+  const multipart = [
+    'preamble',
+    '--boundary',
+    '',
+    '--boundary--',
+    'epilogue',
+  ].join('\r\n')
+
+  expect(() => {
+    parseMultipart(Buffer.from(multipart), 'boundary')
+  }).toThrow()
+})
+
+it('should reject multipart form data with invalid part header-lines', () => {
+  const multipart = [
+    'preamble',
+    '--boundary',
+    '--boundary',
+    '',
+    'foobar',
+    '--boundary--',
+    'epilogue',
+  ].join('\r\n')
+
+  expect(() => {
+    parseMultipart(Buffer.from(multipart), 'boundary')
+  }).toThrow()
+})
+
+it('should reject multipart form data with no parts', () => {
+  const multipart = [
+    'preamble',
+    '--boundary--',
+    'epilogue'
+  ].join('\r\n')
+
+  expect(() => {
+    parseMultipart(Buffer.from(multipart), 'boundary')
+  }).toThrow()
+})
+
+it('should reject multipart form data with no close delimiter', () => {
+  const multipart = [
+    'preamble',
+    '--boundary',
+    '',
+    '',
+    '--boundary',
+    'epilogue',
+  ].join('\r\n')
+
+  expect(() => {
+    parseMultipart(Buffer.from(multipart), 'boundary')
+  }).toThrow()
+})
