f8c0ed33022e66517eae5faa6cb29057df751c86
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-08-25 16:31:39 +0100

    fix: hacky workaround for `iconv` import

--- src/utils/iconv.ts
+++ src/utils/iconv.ts
@@ -1,4 +1,7 @@
-import * as iconv from "iconv-lite"
+import * as _iconv from 'iconv-lite'
+
+// @ts-expect-error
+const iconv = _iconv.default as typeof _iconv 
 
 export const charsetExists = iconv.encodingExists
 export const decodeCharset = iconv.decode
